0cb86308e8243a4542b2c39e53115a3a4f83391e
djpcj86888 <djpcj86888@gmail.com>
2018-11-19 00:32:17 +0800

    first commit

---
+++ Assets/Scripts/Changebutton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;  //DIY
+
+public class Changebutton : MonoBehaviour {
+
+	public void Botton(string name)
+	{
+
+		SceneManager.LoadScene(name, LoadSceneMode.Single);
+	}
+
+
+}
---
+++ Assets/Scripts/ObjectHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHandler : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 vec3 = this.transform.eulerAngles;
+		vec3.y += 20 * Time.deltaTime;
+		vec3.y %= 360;
+
+		//
+		// vec3.x += 20....  //alway = 90
+		// vec3.x %= 90
+		this.transform.eulerAngles = vec3;
+	}
+}
